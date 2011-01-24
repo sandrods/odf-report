7cd928918bb5b46221b63dd2eccf8dccde4f18c1
José Ignacio Costa <jose@wyeworks.com>
2011-01-24 15:41:59 -0200

    Fix requires to run without loading the system gem

--- lib/odf-report.rb
+++ lib/odf-report.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'zip/zipfilesystem'
 require 'fileutils'
-require 'odf-report/file_ops'
-require 'odf-report/hash_gsub'
-require 'odf-report/section'
-require 'odf-report/table'
-require 'odf-report/report'
+require File.expand_path('../odf-report/file_ops',__FILE__)
+require File.expand_path('../odf-report/hash_gsub',__FILE__)
+require File.expand_path('../odf-report/section',__FILE__)
+require File.expand_path('../odf-report/table',__FILE__)
+require File.expand_path('../odf-report/report',__FILE__)
