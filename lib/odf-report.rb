7cc730053701d5c830e7cfe756cb2eaacf43c3fc
Sandro Duarte <sandrods@gmail.com>
2011-09-06 13:09:24 -0300

    space reorg

--- lib/odf-report.rb
+++ lib/odf-report.rb
@@ -3,8 +3,8 @@ require 'zip/zipfilesystem'
 require 'fileutils'
 require 'nokogiri'
 
-require File.expand_path('../odf-report/file_ops',__FILE__)
-require File.expand_path('../odf-report/hash_gsub',__FILE__)
-require File.expand_path('../odf-report/section',__FILE__)
-require File.expand_path('../odf-report/table',__FILE__)
-require File.expand_path('../odf-report/report',__FILE__)
+require File.expand_path('../odf-report/file_ops',  __FILE__)
+require File.expand_path('../odf-report/hash_gsub', __FILE__)
+require File.expand_path('../odf-report/section',   __FILE__)
+require File.expand_path('../odf-report/table',     __FILE__)
+require File.expand_path('../odf-report/report',    __FILE__)
