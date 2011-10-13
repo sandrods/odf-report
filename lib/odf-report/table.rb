90095845e93cdf2db25e8f05fbe81c2f44f4aff0
Matthew Wilson <mattwilsoncp@suddenlink.net>
2011-10-13 09:29:02 -0400

    Tables with <table:table-header-rows> were yielding: can't dup NilClass.

--- lib/odf-report/table.rb
+++ lib/odf-report/table.rb
@@ -37,7 +37,7 @@ class Table
 
     populate!(row)
 
-    @template_rows = table.xpath("table:table-row")
+    @template_rows = table.xpath("table:table-header-rows").xpath("table:table-row") + table.xpath("table:table-row")
 
     @collection.each do |data_item|
 
