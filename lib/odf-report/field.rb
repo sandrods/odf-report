9122487b7d475a3db20e826372c28d38029a55d0
Sandro Duarte <sandrods@gmail.com>
2011-12-29 15:15:52 -0200

    flexibilize data structures ODFReport can handle
    now you can pass objects & hashes

--- lib/odf-report/field.rb
+++ lib/odf-report/field.rb
@@ -6,15 +6,15 @@ class Field
 
   def initialize(opts, &block)
     @name = opts[:name]
+    @data_field = opts[:data_field]
 
     unless @value = opts[:value]
 
-      if data_field = opts[:data_field]
-        @block = lambda { |item| item.send(data_field)}
-      elsif block_given?
+      if block_given?
         @block = block
+
       else
-        @block = lambda { |item| item.send(@name)}
+        @block = lambda { |item| self.extract_value(item) }
       end
 
     end
@@ -34,6 +34,22 @@ class Field
     else
       "#{DELIMITERS}#{@name.to_s.upcase}#{DELIMITERS}"
     end
+  end
+
+  def extract_value(data_item)
+    key = @data_field || @name
+
+    if data_item.is_a?(Hash)
+      data_item[key] || data_item[key.to_s.downcase] || data_item[key.to_s.upcase] || data_item[key.to_s.downcase.to_sym]
+
+    elsif data_item.respond_to?(key.to_s.downcase.to_sym)
+      data_item.send(key.to_s.downcase.to_sym)
+
+    else
+      raise "Can't find field [#{key}] in this #{data_item.class}"
+
+    end
+
   end
 
 end
