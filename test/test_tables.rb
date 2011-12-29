d8297c365e0eefd69f44ab36a04f97c73b55ad66
Sandro Duarte <sandrods@gmail.com>
2011-12-29 15:16:18 -0200

    reflect previous changes in tests

--- test/test_tables.rb
+++ test/test_tables.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 
 col1 = []
 (1..40).each do |i|
-  col1 << OpenStruct.new({:name=>"name #{i}",  :idx=>i,  :address=>"this is address #{i}"})
+  col1 << {:name=>"name #{i}",  :idx=>i,  :address=>"this is address #{i}"}
 end
 
 
@@ -11,7 +11,7 @@ col2 = []
 col2 << OpenStruct.new({:name=>"josh harnet",   :idx=>"02", :address=>"testing <&> ",                 :phone=>99025668, :zip=>"90420-002"})
 col2 << OpenStruct.new({:name=>"sandro duarte", :idx=>"45", :address=>"address with &",               :phone=>88774451, :zip=>"90490-002"})
 col2 << OpenStruct.new({:name=>"ellen bicca",   :idx=>"77", :address=>"<address with escaped html>",  :phone=>77025668, :zip=>"94420-002"})
-col2 << OpenStruct.new({:name=>"luiz garcia",   :idx=>"88", :address=>"address with\nlinebreak",      :phone=>27025668, :zip=>"94520-025"})
+col2 << OpenStruct.new({:name=>"luiz garcia",   'idx'=>"88", :address=>"address with\nlinebreak",      :phone=>27025668, :zip=>"94520-025"})
 
 col3, col4, col5 = [], [], []
 
@@ -25,13 +25,13 @@ report = ODFReport::Report.new("test_tables.odt") do |r|
   r.add_table("TABLE_01", col1, :header=>true) do |t|
     t.add_column(:field_01, :idx)
     t.add_column(:field_02, :name)
-    t.add_column(:field_03, :address)
+    t.add_column(:address)
   end
 
   r.add_table("TABLE_02", col2) do |t|
     t.add_column(:field_04, :idx)
     t.add_column(:field_05, :name)
-    t.add_column(:field_06, :address)
+    t.add_column(:field_06, 'address')
     t.add_column(:field_07, :phone)
     t.add_column(:field_08, :zip)
   end
