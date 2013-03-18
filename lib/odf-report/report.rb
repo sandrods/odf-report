fadb76f98280aaaa7b6b5c88f4942c553e91ca60
Pablo Oldani <oldani.pablo@gmail.com>
2013-03-18 15:39:37 -0300

    add identation and remove un duplicated methods

--- lib/odf-report/report.rb
+++ lib/odf-report/report.rb
@@ -1,114 +1,94 @@
 module ODFReport
 
-class Report < ODFReport::Component
-  include Fields, Images
+  class Report < ODFReport::Component
+    include Fields, Images
 
-  attr_accessor :fields, :tables, :images, :sections, :file, :texts
+    attr_accessor :fields, :tables, :images, :sections, :file, :texts
 
-  def initialize(template_name, &block)
-  
-    @file = ODFReport::File.new(template_name)
+    def initialize(template_name, &block)
+    
+      @file = ODFReport::File.new(template_name)
 
-    @texts = []
-    @fields = []
-    @tables = []
-    @images = {}
-    @image_names_replacements = {}
-    @sections = []
+      @texts = []
+      @fields = []
+      @tables = []
+      @images = {}
+      @image_names_replacements = {}
+      @sections = []
 
-    yield(self)
+      yield(self)
 
-  end
+    end
 
-  def add_field(field_tag, value='', &block)
-    opts = {:name => field_tag, :value => value}
-    field = Field.new(opts, &block)
-    @fields << field
-  end
+    def add_image(name, path)
+      @images[name] = path
+    end
 
-  def add_text(field_tag, value='', &block)
-    opts = {:name => field_tag, :value => value}
-    text = Text.new(opts)
-    @texts << text
-  end
+    def generate(dest = nil, &block)
 
-  def add_table(table_name, collection, opts={}, &block)
-    opts.merge!(:name => table_name, :collection => collection)
-    tab = Table.new(opts)
-    @tables << tab
+      @file.create(dest)
 
-    yield(tab)
-  end
+      @file.update('content.xml', 'styles.xml') do |txt|
 
-  def add_image(name, path)
-    @images[name] = path
-  end
+        parse_document(txt) do |doc|
 
-  def generate(dest = nil, &block)
+          replace_fields!(doc)
+          replace_texts!(doc)
 
-    @file.create(dest)
+          replace_sections!(doc)
+          replace_tables!(doc)
 
-    @file.update('content.xml', 'styles.xml') do |txt|
+          find_image_name_matches(doc)
 
-      parse_document(txt) do |doc|
-
-        replace_fields!(doc)
-        replace_texts!(doc)
-
-        replace_sections!(doc)
-        replace_tables!(doc)
-
-        find_image_name_matches(doc)
+        end
 
       end
 
+      replace_images(@file)
+
+      if block_given?
+        yield @file.path
+        @file.remove
+      end
+
+      @file.path
+
     end
 
-    replace_images(@file)
-
-    if block_given?
-      yield @file.path
+    def cleanup
       @file.remove
     end
 
-    @file.path
+  private
 
-  end
-
-  def cleanup
-    @file.remove
-  end
-
-private
-
-  def parse_document(txt)
-    doc = Nokogiri::XML(txt)
-    yield doc
-    txt.replace(doc.to_s)
-  end
-
-  def replace_fields!(content)
-    field_replace!(content)
-  end
-
-  def replace_texts!(content)
-    @texts.each do |text|
-      text.replace!(content)
+    def parse_document(txt)
+      doc = Nokogiri::XML(txt)
+      yield doc
+      txt.replace(doc.to_s)
     end
-  end
 
-  def replace_tables!(content)
-    @tables.each do |table|
-      table.replace!(content)
+    def replace_fields!(content)
+      field_replace!(content)
     end
-  end
 
-  def replace_sections!(content)
-    @sections.each do |section|
-      section.replace!(content)
+    def replace_texts!(content)
+      @texts.each do |text|
+        text.replace!(content)
+      end
     end
-  end
 
-end
+    def replace_tables!(content)
+      @tables.each do |table|
+        table.replace!(content)
+      end
+    end
+
+    def replace_sections!(content)
+      @sections.each do |section|
+        section.replace!(content)
+      end
+    end
+
+  end
 
 end
