66f26330f248066451f1bf6834c1fc8943997d0a
Mauricio Menegaz <mauricio.menegaz@gmail.com>
2024-03-21 15:02:37 -0300

    Use nokogiri's HTML5 parser for text nodes (#129)
    * use nokogiri's HTML5 parser for text nodes
    
    * bump up version

--- lib/odf-report/version.rb
+++ lib/odf-report/version.rb
@@ -1,3 +1,3 @@
 module ODFReport
-  VERSION = "0.7.3"
+  VERSION = "0.8.0"
 end
---
+++ spec/texts_spec.rb
@@ -0,0 +1,48 @@
+RSpec.describe "Texts" do
+   before(:context) do
+
+    @text1 = <<-HTML
+      <p>This is some text in a paragraph</p>
+    HTML
+
+    @text2 = <<-HTML
+      <p>Text before line break <br> text after line break</p>
+    HTML
+
+    @text3 = <<-HTML
+      <p>Text before entities
+         Maur&iacute;cio
+         Text after entities</p>
+    HTML
+
+
+    report = ODFReport::Report.new("spec/templates/specs.odt") do |r|
+
+      r.add_text(:text1, @text1)
+      r.add_text(:text2, @text2)
+      r.add_text(:text3, @text3)
+
+    end
+
+    report.generate("spec/result/specs.odt")
+
+    @data = Inspector.new("spec/result/specs.odt")
+
+  end
+
+  it "simple text replacement" do
+    expect(@data.text).to include "This is some text in a paragraph"
+  end
+
+  it "text replacement with <br>" do
+    expect(@data.text).to include "Text before line break"
+    expect(@data.text).to include "text after line break"
+  end
+
+  it "text replacement with html entities" do
+    expect(@data.text).to include "Text before entities"
+    expect(@data.text).to include "Maurício"
+    expect(@data.text).to include "Text after entities"
+  end
+
+end
